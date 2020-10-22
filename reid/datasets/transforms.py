878689b04ea6ae9e36320507f7834ec611434e16
wangjq4214 <w1637894214@163.com>
2020-10-22 17:20:34 +0800

    feat: 完成部分转换函数

---
+++ reid/datasets/transforms.py
@@ -0,0 +1,61 @@
+import random
+import math
+
+from PIL import Image
+
+# 这个写法可以学习, python 函数柯里化的一个写法
+
+
+class RectScale:
+    """
+    图片 resize
+    """
+
+    def __init__(self, height, width, interpolation=Image.BILINEAR):
+        self.height = height
+        self.width = width
+        self.interpolation = interpolation
+
+    def __call__(self, img):
+        w, h = img.size
+        if h == self.height and w == self.width:
+            return img
+        return img.resize((self.width, self.height), self.interpolation)
+
+
+class RandomSizedRectCrop:
+    """
+    图片随机裁剪和 resize
+    """
+
+    def __init__(self, height, width, interpolation=Image.BILINEAR):
+        self.height = height
+        self.width = width
+        self.interpolation = interpolation
+
+    def __call__(self, img):
+        for _ in range(10):
+            # 进行尝试, 可能不成功
+            area = img.size[0] * img.size[1]
+            target_area = random.uniform(0.64, 1.0) * area
+            aspect_ratio = random.uniform(2, 3)
+
+            h = int(round(math.sqrt(target_area * aspect_ratio)))
+            w = int(round(math.sqrt(target_area / aspect_ratio)))
+
+            if w <= img.size[0] and h <= img.size[1]:
+                x1 = random.randint(0, img.size[0] - w)
+                y1 = random.randint(0, img.size[1] - h)
+
+                img = img.crop((x1, y1, x1 + w, y1 + h))
+                assert(img.size == (w, h))
+
+                return img.resize((self.width, self.height), self.interpolation)
+
+        # 如果不成功直接 resize
+        scale = RectScale(self.height, self.width, self.interpolation)
+        return scale(img)
+
+
+class RandomSizedEarser:
+    pass
