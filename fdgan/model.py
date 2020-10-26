33d11d3b63a2d451899a7daa4a674c07b087fba1
wangjq4214 <w1637894214@163.com>
2020-10-26 16:48:55 +0800

    feat: fdgan init

---
+++ fdgan/model.py
@@ -0,0 +1,4 @@
+class FDGANModel:
+    def __init__(self, opt) -> None:
+        self.opt = opt
+        
---
+++ fdgan/network.py
@@ -0,0 +1,34 @@
+import functools
+
+from torch import nn
+from torch.nn import init
+
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        init.normal(m.weight.data, 0.0, 0.02)
+    elif classname.find('Linear') != -1:
+        init.normal(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm2d') != -1:
+        init.normal(m.weight.data, 1.0, 0.02)
+        init.constant(m.bias.data, 0.0)
+
+
+def init_weights(net):
+    net.apply(weights_init_normal)
+
+
+def get_norm_layer(norm_type='batch'):
+    if norm_type == 'batch':
+        norm_layer = functools.partial(nn.BatchNorm2d, affine=True)
+    elif norm_type == 'instance':
+        norm_layer = functools.partial(nn.InstanceNorm2d, affine=False)
+    elif norm_type == 'none':
+        norm_layer = None
+    else:
+        raise NotImplementedError(
+            f'normalization layer [{norm_type}] is not found')
+    return norm_layer
+
+
